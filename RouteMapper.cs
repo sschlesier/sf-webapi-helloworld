994309a9214e870cc8b92117b9edf9a0dc8fed8b
Scott Schlesier <scott@schlesier.ca>
2012-09-05 17:21:36 -0700

    initial commit

---
+++ RouteMapper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using DotNetNuke.Web.Api;
+
+namespace MyServices
+{
+    class RouteMapper : IServiceRouteMapper
+    {
+        public void RegisterRoutes(IMapRoute mapRouteManager)
+        {
+            mapRouteManager.MapHttpRoute("MyServices", "{controller}/{action}", new[] {"MyServices"});
+        }
+    }
+}
---
+++ WelcomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DotNetNuke.Web.Api;
+
+namespace MyServices
+{
+    class WelcomeController : DnnApiController
+    {
+        [AllowAnonymous]
+        public HttpResponseMessage HelloWorld()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, "Hello World!");
+        }
+    }
+}
